c0699dde4ec150a7b364e85eb1d1d71ca746f879
Anna Lyubinska <anna.lyubinskagmail.com>
2025-09-19 22:44:38 +0200

    Solution

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
-FROM python:3.13-alpine3.21
+FROM python:3.10.8-slim
 
 LABEL maintainer="lyubinska"
 
 ENV PYTHONUNBUFFERED 1
 
-WORKDIR app/
+WORKDIR /app
 
-COPY requirements.txt requirements.txt
+COPY requirements.txt .
 
 RUN pip install -r requirements.txt
 
--- app/main.py
+++ app/main.py
@@ -9,18 +9,21 @@ URL = "http://api.weatherapi.com/v1/current.json"
 def get_weather() -> None:
     load_dotenv()
     api_key = os.getenv("API_KEY")
+    if not api_key:
+        raise Exception("API_KEY environment variable not set")
+
     params = {"key": api_key, "q": CITY}
 
     response = requests.get(URL, params=params)
     data = response.json()
-    if response.status_code == 200:
-        print(f"{data['location']['name']}/{data['location']['country']}",
+    if response.status_code != 200:
+        raise Exception(response.text)
+
+    print(f"{data['location']['name']}/{data['location']['country']}",
               f"\nlocaltime: {data['location']['localtime']}",
               f"\nWeather: {data['current']['temp_c']} C, "
               f"\nHumidity: {data['current']['humidity']}"
               f"\nCondition: {data['current']['condition']['text']}")
-    else:
-        print(f"{response.status_code}/{data['error']['message']}")
 
 
 if __name__ == "__main__":
